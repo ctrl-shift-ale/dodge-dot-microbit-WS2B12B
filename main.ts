ead3f01531dfe0d8f23aa3c085c28442d5b19b0d
Alessandro Quaranta <79669123+ctrl-shift-ale@users.noreply.github.com>
2026-06-08 01:22:27 +0100

    Added background (scrolling, tiles - mine and healing). Breakdown of init in several functions

--- main.ts
+++ main.ts
@@ -1,5 +1,42 @@
 function update_background () {
-	
+    row = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
+    ]
+    empty_row = !(empty_row)
+    if (empty_row == false) {
+        n_mines = randint(1, 3)
+        mines_pos = []
+        for (let index = 0; index <= n_mines - 1; index++) {
+            done = false
+            while (!(done)) {
+                pos = randint(0, 7)
+                if (mines_pos.indexOf(pos) == -1) {
+                    mines_pos.push(pos)
+                    done = true
+                }
+            }
+        }
+        for (let value of mines_pos) {
+            if (randint(1, 100) > healing_tile_prob) {
+                row[value] = 1
+            } else {
+                row[value] = 2
+            }
+        }
+    }
+    for (let value of row) {
+        background.push(value)
+    }
+    for (let index2 = 0; index2 < 8; index2++) {
+        background.shift()
+    }
 }
 function update_velocity_x () {
     acceleration_x = input.acceleration(Dimension.X)
@@ -10,7 +47,7 @@ function update_velocity_x () {
         friction = 0.9
     }
     serial.writeValue("acceleration x", acceleration_x)
-    velocity_x = velocity_x * friction + acceleration_x * SPEED_TIMESTEP
+    velocity_x = velocity_x * friction + acceleration_x
     if (Math.abs(velocity_x) < 10) {
         velocity_x = 0
     }
@@ -47,16 +84,68 @@ function update_player () {
     serial.writeValue("x", player_current_x_pos)
 }
 function draw_background () {
-	
+    for (let led_index = 0; led_index <= background.length - 1; led_index++) {
+        tile_brightness = 40
+        if (background[led_index] == 1) {
+            tile_colour = PURPLE
+        } else if (background[led_index] == 2) {
+            tile_colour = YELLOW
+        } else {
+            tile_brightness = 0
+        }
+        index = DISPLAY.range(led_index, 1)
+        index.showColor(neopixel.hsl(tile_colour, 50, tile_brightness))
+    }
+}
+function init_tilt () {
+    tilt_accum = 0
+    tilt = 0
+    HOR_TILT_DEADZONE = 100
+    MAX_VERTICAL_TILT = 400
+    friction = 0.66
+}
+function init_display () {
+    BRIGHTNESS_OFF = 0
+    RED = 0
+    ORANGE = 40
+    GREEN = 120
+    BLUE = 240
+    YELLOW = 60
+    CYAN = 180
+    AQUAMARINE = 160
+    PURPLE = 300
+    BRIGHTNESS_MAX = 50
+    BRIGHTNESS_PLAYER_MIN = 30
+    BRIGHTNESS_PLAYER_MAX = 40
 }
 function update_velocity_y () {
+    let PLAYER_SPEED_TIMESTEP = 0
     acceleration_y = input.acceleration(Dimension.Y) * -1
     acceleration_y = Math.constrain(acceleration_y, MAX_VERTICAL_TILT * -1, MAX_VERTICAL_TILT)
     acceleration_y = acceleration_y * 3
     serial.writeValue("accel y", acceleration_y)
-    scrolling_timestep_ms = scrolling_timestep_ms * friction + acceleration_y * SPEED_TIMESTEP
+    scrolling_timestep_ms = scrolling_timestep_ms * friction + acceleration_y * PLAYER_SPEED_TIMESTEP
     serial.writeValue("scrolling time", scrolling_timestep_ms)
-    scrolling_timestep_ms = Math.map(scrolling_timestep_ms, 355, 2020, scrolling_timestep_max_ms, scrolling_timestep_min_ms)
+    scrolling_timestep_ms = Math.map(scrolling_timestep_ms, 355, 2020, SCROLLING_TIMESTEP_MAX_MS, SCROLLING_TIMESTEP_MIN_MS)
+}
+function init_background () {
+    scrolling_timestep_ms = 1000
+    SCROLLING_TIMESTEP_MIN_MS = 2000
+    SCROLLING_TIMESTEP_MAX_MS = 500
+    scrolling_refresh = 0
+    healing_tile_prob = 2
+    scrolling_refresh = 0
+    empty_row = true
+}
+function init_player () {
+    REFRESH_PLAYER_TILE_MS = 100
+    PLAYER_SPEED_TIMESTEP_MS = 1000
+    player_current_brightness = BRIGHTNESS_PLAYER_MIN
+    player_current_colour = BLUE
+    player_current_x_pos = 3000
+    player_index_past = 3
+    PLAYER_MAX_SPEED = 14000
+    max_velocity_per_timestep = PLAYER_MAX_SPEED / (1000 / PLAYER_SPEED_TIMESTEP_MS)
 }
 /**
  * tilt range (-1023, 1023)
@@ -64,71 +153,68 @@ function update_velocity_y () {
  * max dot speed = 2 dots per sec
  */
 let last_dot_refresh = 0
-let refresh_display = false
-let acceleration_y = 0
-let player_prev_x_pos = 0
-let cur_brightness = 0
-let compressed_velocity_for_led_colouring = 0
-let index: neopixel.Strip = null
-let led_index = 0
-let velocity_x = 0
-let acceleration_x = 0
-let DISPLAY: neopixel.Strip = null
-let friction = 0
-let SPEED_TIMESTEP_MS = 0
-let max_velocity_per_timestep = 0
-let player_current_x_pos = 0
-let player_current_colour = 0
-let player_current_brightness = 0
-let scrolling_timestep_max_ms = 0
-let scrolling_timestep_min_ms = 0
-let scrolling_timestep_ms = 0
-let SPEED_TIMESTEP = 0
-let BRIGHTNESS_PLAYER_MAX = 0
-let BRIGHTNESS_PLAYER_MIN = 0
-let AQUAMARINE = 0
-let BLUE = 0
-let GREEN = 0
-let ORANGE = 0
-let RED = 0
-let MAX_VERTICAL_TILT = 0
-let HOR_TILT_DEADZONE = 0
-let ACTIVE = false
-ACTIVE = false
-let CENTRE_POS = [3000, 3000]
-HOR_TILT_DEADZONE = 100
-MAX_VERTICAL_TILT = 400
-RED = 0
-ORANGE = 40
-GREEN = 120
-BLUE = 240
-let YELLOW = 60
-let CYAN = 180
-AQUAMARINE = 160
-let PURPLE = 300
-let BRIGHTNESS_MAX = 50
-let BRIGHTNESS_OFF = 0
-BRIGHTNESS_PLAYER_MIN = 30
-BRIGHTNESS_PLAYER_MAX = 40
-SPEED_TIMESTEP = 0.1
-scrolling_timestep_ms = 1000
-scrolling_timestep_min_ms = 2000
-scrolling_timestep_max_ms = 500
-let scrolling_refresh = 0
-player_current_brightness = BRIGHTNESS_PLAYER_MIN
-player_current_colour = BLUE
-player_current_x_pos = 3000
-let player_index_past = 3
-let REFRESH_DOT_MS = 100
-let DOT_MAX_SPEED_UNIT_PER_SEC = 14000
-max_velocity_per_timestep = DOT_MAX_SPEED_UNIT_PER_SEC / (1000 / SPEED_TIMESTEP_MS)
 let last_tilt_refresh = 0
+let refresh_display = false
+let PLAYER_MAX_SPEED = 0
+let player_index_past = 0
+let PLAYER_SPEED_TIMESTEP_MS = 0
+let REFRESH_PLAYER_TILE_MS = 0
+let scrolling_refresh = 0
+let SCROLLING_TIMESTEP_MIN_MS = 0
+let SCROLLING_TIMESTEP_MAX_MS = 0
+let scrolling_timestep_ms = 0
+let acceleration_y = 0
+let BRIGHTNESS_MAX = 0
+let CYAN = 0
+let BRIGHTNESS_OFF = 0
+let MAX_VERTICAL_TILT = 0
 let tilt = 0
 let tilt_accum = 0
-friction = 0.66
+let YELLOW = 0
+let PURPLE = 0
+let tile_colour = 0
+let tile_brightness = 0
+let player_prev_x_pos = 0
+let BRIGHTNESS_PLAYER_MAX = 0
+let BRIGHTNESS_PLAYER_MIN = 0
+let cur_brightness = 0
+let BLUE = 0
+let GREEN = 0
+let AQUAMARINE = 0
+let ORANGE = 0
+let RED = 0
+let compressed_velocity_for_led_colouring = 0
+let player_current_brightness = 0
+let player_current_colour = 0
+let index: neopixel.Strip = null
+let led_index = 0
+let player_current_x_pos = 0
+let max_velocity_per_timestep = 0
+let velocity_x = 0
+let friction = 0
+let HOR_TILT_DEADZONE = 0
+let acceleration_x = 0
+let healing_tile_prob = 0
+let pos = 0
+let done = false
+let mines_pos: number[] = []
+let n_mines = 0
+let empty_row = false
+let row: number[] = []
+let DISPLAY: neopixel.Strip = null
+let background: number[] = []
+let ACTIVE = false
+ACTIVE = false
+background = []
+for (let index2 = 0; index2 < 64; index2++) {
+    background.push(0)
+}
+init_tilt()
+init_display()
+init_player()
+init_background()
 DISPLAY = neopixel.create(DigitalPin.P0, 64, NeoPixelMode.RGB_RGB)
 let single_led = DISPLAY.range(0, 1)
-let empty_row = true
 basic.forever(function () {
     refresh_display = false
     if (ACTIVE) {
@@ -137,7 +223,7 @@ basic.forever(function () {
             update_velocity_y()
             last_tilt_refresh = control.millis()
         }
-        if (control.millis() - last_dot_refresh >= REFRESH_DOT_MS) {
+        if (control.millis() - last_dot_refresh >= REFRESH_PLAYER_TILE_MS) {
             DISPLAY.clear()
             update_player()
             draw_background()
@@ -145,6 +231,14 @@ basic.forever(function () {
             refresh_display = true
             last_dot_refresh = control.millis()
             last_dot_refresh = control.millis()
+        }
+        if (control.millis() - scrolling_refresh >= scrolling_timestep_ms) {
+            DISPLAY.clear()
+            update_background()
+            draw_background()
+            set_player_led()
+            DISPLAY.show()
+            scrolling_refresh = control.millis()
         }
     }
     if (refresh_display) {
